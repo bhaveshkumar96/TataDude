e41e661659d5461783d0c63418a00e1eea8f5918
BhaveshKumar <bk769221@gmail.com>
2023-05-25 19:50:20 +0530

    working on another page

--- tatadude/src/App.js
+++ tatadude/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Friends from './pages/Friends';
+import RefferalPage from './pages/RefferalPage';
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar/> */}
     {/* <Friends/> */}
+    <RefferalPage/>
     </div>
   );
 }
--- tatadude/src/pages/RefferalPage.jsx
+++ tatadude/src/pages/RefferalPage.jsx
@@ -1,17 +1,32 @@
+import "../styles/RefferalPage.css"
 const RefferalPage = () => {
   return (
     <div>
       <div id="container">
-        <div>
+        <div id="cont-1st-div"> 
           <div>
            <p>Referral Earning</p>
            <h2>₹ 2,500</h2>
           </div>
-          <div></div>
-          <div></div>
+          <div>
+            <p>Total Referrals</p>
+            <h2>7</h2>
+          </div>
+          <div>
+            <p>Wallet Balance</p>
+            <h2>₹ 500</h2>
+
+          </div>
+          
+            <button id="wid-button">Withdraw Balance</button>
+     
         </div>
-        <div></div>
+        <div id="cont-2nd-div">
+          <h2>Your Referral Code </h2>
+          <div>EDCH54</div>
+        </div>
       </div>
+      
     </div>
   );
 };
