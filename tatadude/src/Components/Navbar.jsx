61ddcb3d08781e0a8b83038839318db10d53e526
BhaveshKumar <bk769221@gmail.com>
2023-05-26 12:08:02 +0530

    Final commit

--- tatadude/src/Components/Navbar.jsx
+++ tatadude/src/Components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
 import { BiUserCircle } from 'react-icons/bi';
 const Navbar = () => {
   return (
     <nav>
       <div className="navbar-logo">
+        <Link to={"/"}>
         <img src="https://www.tutedude.com/refundassets/Tutedude.png" alt="logo" />
+        </Link>
       </div>
       <div className="navbar-menu">
        
